1077436ccd8dfb7d062d2f4aeb4987ffbd773b9d
heilelonmusk <heil@helon.space>
2025-02-24 01:08:21 +0100

    atlas

--- database/mongoConnection.js
+++ database/mongoConnection.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
@@ -6,9 +7,10 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB Connected");
+
+    console.log("✅ MongoDB Connected to Atlas");
   } catch (error) {
-    console.error("MongoDB Connection Failed:", error);
+    console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
